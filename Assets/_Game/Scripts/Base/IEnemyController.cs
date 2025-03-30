f5a6e923aab549d14ba0c142091895cd0b71e81e
Thang <sgooophx@gmail.com>
2025-03-30 14:31:59 +0700

    save transform investigating

---
+++ Assets/_Game/Scripts/Base/IEnemyController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets._Game.Scripts
+{
+    internal interface IEnemyController
+    {
+        public void OnSpawnEnemy();
+        public void OnDestroyEnemy();
+
+    }
+}
---
+++ Assets/_Game/Scripts/ObjectControl/Enemies/CommonCarController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets._Game.Scripts.ObjectControl.Enemies
+{
+    internal class CommonCarController : MonoBehaviour, IEnemyController
+    {
+        private float moveSpeed = 5f;
+        private Rigidbody2D rb;
+        public void OnDestroyEnemy()
+        {
+            Debug.Log("onDestroy");
+        }
+
+        public void OnSpawnEnemy()
+        {
+            Debug.Log("onSpawn");
+        }
+
+        void Start()
+        {
+
+        }
+        void Update()
+        {
+        
+        }
+    }
+}
